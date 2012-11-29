b7206990dacd57bcdf4a771e0d995c9c7ac3ecc9
tie <tie@TTOP.(none)>
2012-11-29 17:27:17 +0200

    Changing packaging

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from distutils.core import setup
 
 setup(
     name='PyLogWatch',
-    version='0.1.0',
+    version='0.1.0-alpha',
     author='E. Filipov, MTR Design',
     author_email='pylogwatch@mtr-design.com',
-    packages=['pylogwatch'],
+    packages=['pylogwatch','pylogwatch.formatters'],
     scripts=['bin/pylogwatch.py'],
     url='http://pypi.python.org/pypi/PyLogWatch/',
     license='LICENSE.txt',
     description='Python utility to parse log files and send them to a Sentry server.',
     long_description=open('README.txt').read(),
+    install_requires=[
+        "python-dateutil == 1.5",
+        "raven >= 2.0",
+    ],
 )
